5f51d3af0b2e79a59b0dceb703f72073def170f8
Chris-Ru <72892271+Chris-Ru@users.noreply.github.com>
2020-10-24 17:46:12 -0700

    New update (10/24/2020)

---
+++ query.py
@@ -0,0 +1,46 @@
+# CODE IN THIS FILE WRITTEN BY KIAN
+"""
+from pyquery import PyQuery as pq
+name = input("What do you want to search?")
+url="https://www.amazon.com/s?k={name}&ref=nb_sb_noss"
+response = pq(url=url.format(name=name))
+print(response)"""
+
+import requests
+from bs4 import BeautifulSoup
+
+query = input("What product do you want to search for?")
+URL = 'https://www.etsy.com/search/?query=' + query
+#URL = 'https://www.amazon.com/s?k=' + query + '&ref=nb_sb_noss_2'
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+open("test.txt", "w").write(str(soup))
+#print(soup)
+"""
+x = 0
+while True:
+ try: 
+    from googlesearch import search 
+ except ImportError:  
+    print("No module named 'google' found") 
+ query = input("Google: ")
+ x = 1
+ y = 2
+ for j in search(query, tld="co.in", num=10, stop=1, pause=2):
+   req = requests.get(j)
+   soup = BeautifulSoup(req.text, "lxml")
+   title = soup.title.string
+   if title == "403 Forbidden":
+     title = "Blocked Title"
+   if title == "Sorry! Something went wrong!":
+     title = "Error: Title Not Found"
+   print("")
+   print(title)
+   print(j) 
+   print("")
+   x += 1
+ print("")
+ print("|--------------------------------------------------------------------------------------------|")
+ print("")
+ search(query, tld='com', lang='en', num=10, start=0, stop=None, pause=2.0) """
