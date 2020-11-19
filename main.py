92f4f2834d8819a10daa4f7373e9f5203b472b11
zenxha <komays5042@gmail.com>
2020-11-18 16:26:57 -0800

    delete files

--- main.py
+++ main.py
@@ -96,4 +96,4 @@ def popularitems():
 
 if __name__ == "__main__":
     #runs the application on the repl development server
-    app.run(debug=True,host='0.0.0.0',port=5000)
+    app.run(debug=True)
